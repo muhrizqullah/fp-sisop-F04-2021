42953dfddcc9843ce2e70a2238ac169759bf777b
Misdinar <55637655+Misdinar@users.noreply.github.com>
2021-06-18 12:14:09 +0800

    bisa create & drop database

--- client/client_database.c
+++ client/client_database.c
@@ -6,17 +6,65 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #define PORT 8080
-  
+
+char pesan[50]={0};
+
+void sqlCreate(int sock){
+    char namaDB[50]="create_db";
+    strcat(namaDB, pesan+15);
+    printf("%s\n",namaDB);
+    send(sock,namaDB,strlen(namaDB),0);
+    
+}
+
+void sqlDrop(int sock){
+    char namaDB[50]="drop_db";
+    strcat(namaDB, pesan+13);
+    printf("%s\n",namaDB);
+    send(sock,namaDB,strlen(namaDB),0);
+    
+}
+
 int main(int argc, char const *argv[]) {
-    int valread;
+    struct sockaddr_in address;
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
     char *hello = "Hello from client";
     char buffer[1024] = {0};
-    
-    int sock = create_socket();
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+  
+    memset(&serv_addr, '0', sizeof(serv_addr));
+  
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+      
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+  
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
 
-    send(sock , hello , strlen(hello) , 0 );
-    printf("Hello message sent: %s\n", argv[0]);
-    valread = read( sock , buffer, 1024);
-    printf("%s\n",buffer );
+    // fungsi yg dipakai
+    while(1){
+        gets(pesan);
+        // printf("%s", pesan);
+        char *token;
+        token = strtok(pesan, " ");
+        printf("%s", pesan);
+        if(strcmp("CREATE", pesan)==0){
+            sqlCreate(sock);
+        }
+        else if (strcmp("DROP", pesan)==0){
+            sqlDrop(sock);
+        }
+        else send(sock,"gagal",strlen("gagal"),0);
+    }
     return 0;
 }
